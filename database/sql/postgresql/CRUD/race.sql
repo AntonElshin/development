65b22fee2fa4ba572a33144cd94c2e1688f90dc3
Anton Elshin <anton.elshin@mail.ru>
2025-03-04 22:12:49 +0300

    add CRUD operations for race

---
+++ database/sql/postgresql/CRUD/race.sql
@@ -0,0 +1,323 @@
+-- CRUD (Create Read Update Delete) for race
+
+-- inserts (Create)
+
+-- добавить запись со всеми полями
+
+insert into race (is_active, race_date, race_name, race_city, race_address, description) values
+    (true, '2025-02-22 12:00:00.000000', 'Забег настоящих мужчин', 'Москва', 'Перовский сквер', null);
+
+-- добавить запись только с ненулевыми полями
+
+insert into race (is_active, race_date, race_name, race_city, race_address) values
+    (true, '2025-02-22 12:00:00.000000', 'Забег настоящих мужчин', 'Москва', 'Перовский сквер');
+
+-- selects (Read)
+
+-- найти все записи
+
+select
+    *
+from
+    race;
+
+-- найти по идентификатору
+
+select
+    *
+from
+    race
+where
+    id = 1;
+
+-- найти по параметрам
+
+/*
+  Если для поиска будет четыре отдельных поля:
+    - признак активности
+    - дата забега
+    - название забега
+    - город забега
+*/
+
+-- найти по признаку активности
+
+select
+    *
+from
+    race
+where
+    is_active = true;
+
+-- найти по дате забега через >= и через <=
+
+select
+    *
+from
+    race
+where
+    race_date >= '2025-02-22 00:00:00.000000'
+    and race_date <= '2025-02-22 23:59:59.999999';
+
+-- найти по дате забега через between
+
+select
+    *
+from
+    race
+where
+    race_date between '2025-02-22 00:00:00.000000' and '2025-02-22 23:59:59.999999';
+
+-- найти по названию, приводя всё к нижнему регистру через lower
+
+select
+    *
+from
+    race
+where
+    lower(race_name) like '%' || lower('эст') || '%';
+
+-- найти по городу, приводя всё к нижнему регистру через lower
+
+select
+    *
+from
+    race
+where
+    lower(race_city) like '%' || lower('мос') || '%';
+
+-- найти по признаку активности и дате
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and race_date between '2025-03-01 00:00:00.000000' and '2025-03-01 23:59:59.999999';
+
+-- найти по признаку активности и названию
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and lower(race_name) like '%' || lower('эст') || '%';
+
+-- найти по признаку активности и городу
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and lower(race_city) like '%' || lower('мос') || '%';
+
+-- найти по признаку активности, дате и названию
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and race_date between '2025-03-01 00:00:00.000000' and '2025-03-01 23:59:59.999999'
+    and lower(race_name) like '%' || lower('эст') || '%';
+
+-- найти по признаку активности, дате и городу
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and race_date between '2025-02-22 00:00:00.000000' and '2025-02-22 23:59:59.999999'
+    and lower(race_city) like '%' || lower('мос') || '%';
+
+-- найти по признаку активности, дате, названию и городу
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and race_date between '2025-03-01 00:00:00.000000' and '2025-03-01 23:59:59.999999'
+    and lower(race_name) like '%' || lower('эст') || '%'
+    and lower(race_city) like '%' || lower('мос') || '%';
+
+-- все остальные комбинации полей для поиска
+
+/*
+  Если для поиска будет три отдельных поля:
+    - признак активности
+    - дата забега
+    - строка поиска (название забега или город забега)
+*/
+
+-- найти по признаку активности
+
+select
+    *
+from
+    race
+where
+    is_active = true;
+
+-- найти по дате забега через >= и через <=
+
+select
+    *
+from
+    race
+where
+    race_date >= '2025-02-22 00:00:00.000000'
+    and race_date <= '2025-02-22 23:59:59.999999';
+
+-- или по дате забега через between
+
+select
+    *
+from
+    race
+where
+    race_date between '2025-02-22 00:00:00.000000' and '2025-02-22 23:59:59.999999';
+
+-- найти по строке поиска
+
+select
+    *
+from
+    race
+where
+    lower(race_name) like '%' || lower('а') || '%'
+    or lower(race_city) like '%' || lower('а') || '%';
+
+-- найти по признаку активности и дате
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and race_date between '2025-02-22 00:00:00.000000' and '2025-02-22 23:59:59.999999';
+
+-- найти по признаку активности и строке поиска
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and (
+        lower(race_name) like '%' || lower('а') || '%'
+        or lower(race_city) like '%' || lower('а') || '%'
+    );
+
+-- найти по признаку активности, дате и строке поиска
+
+select
+    *
+from
+    race
+where
+    is_active = true
+    and race_date between '2025-02-22 00:00:00.000000' and '2025-02-22 23:59:59.999999'
+    and (
+        lower(race_name) like '%' || lower('а') || '%'
+        or lower(race_city) like '%' || lower('а') || '%'
+    );
+
+-- все остальные комбинации полей для поиска
+
+-- updates (Update)
+
+-- обновляем все записи
+
+update race
+set
+    is_active = true,
+    race_date = '2025-02-22 12:00:00.000000',
+    race_name = 'Забег настоящих мужчин',
+    race_city = 'Москва',
+    race_address = 'Перовский сквер',
+    description = null;
+
+-- обновляем все поля одной записи по идентификатору
+
+update race
+set
+    is_active = true,
+    race_date = '2025-02-22 12:00:00.000000',
+    race_name = 'Забег настоящих мужчин',
+    race_city = 'Москва',
+    race_address = 'Перовский сквер',
+    description = null
+where
+    id = 1;
+
+-- обновляем признак активности забега одной записи по идентификатору
+
+update race
+set
+    is_active = true
+where
+    id = 1;
+
+-- обновляем дату
+
+update race
+set
+    race_date = '2025-02-22 12:00:00.000000'
+where
+    id = 1;
+
+-- обновляем название забега одной записи по идентификатору
+
+update race
+set
+    race_name = 'Забег настоящих мужчин'
+where
+    id = 1;
+
+-- обновляем город забега одной записи по идентификатору
+
+update race
+set
+    race_city = 'Москва'
+where
+    id = 1;
+
+-- обновляем адрес забега одной записи по идентификатору
+
+update race
+set
+    race_address = 'Перовский сквер'
+where
+    id = 1;
+
+-- обновляем описание забега одной записи по идентификатору
+
+update race
+set
+    description = null
+where
+    id = 1;
+
+-- обновляем все комбинации полей: 2 пары, 3 пары, 4 пары одной записи по идентификатору
+
+-- deletes (Delete)
+
+-- удаляем все записи
+
+delete from race;
+
+-- удаляем запись по идентификатору
+
+delete from race where id = 1;
