4395130614014b9e8f785644d35f8ca9b637ca51
Anton Elshin <anton.elshin@mail.ru>
2025-03-04 19:46:48 +0300

    add comments to tables and columns

--- database/sql/postgresql/DDL.sql
+++ database/sql/postgresql/DDL.sql
@@ -41,6 +41,15 @@ create table if not exists race
     description text
 );
 
+comment on table race is 'Забег';
+comment on column race.id is 'Идентификатор забега';
+comment on column race.is_active is 'Признак активности забега';
+comment on column race.race_date is 'Дата забега';
+comment on column race.race_name is 'Название забега';
+comment on column race.race_city is 'Город забега';
+comment on column race.race_address is 'Адрес забега';
+comment on column race.description is 'Описание забега';
+
 -- create table distance
 
 create table if not exists distance
@@ -51,6 +60,13 @@ create table if not exists distance
     entrance_currency varchar(3),
     racer_limit integer
 );
+
+comment on table distance is 'Дистанция забега';
+comment on column distance.id is 'Идентификатор дистанции';
+comment on column distance.distance_name is 'Название дистанции';
+comment on column distance.entrance_fee is 'Сумма вступительного взноса';
+comment on column distance.entrance_currency is 'Валюта вступительного взноса';
+comment on column distance.racer_limit is 'Лимит участников';
 
 -- create table registration
 
@@ -68,6 +84,19 @@ create table if not exists registration
     mobile_phone varchar(10),
     delete_reason varchar(20)
 );
+
+comment on table registration is 'Регистрация на дистанцию забега';
+comment on column registration.id is 'Идентификатор регистрации';
+comment on column registration.creation_date is 'Дата создания регистрации';
+comment on column registration.status is 'Статус регистрации';
+comment on column registration.last_name is 'Фамилия участника';
+comment on column registration.first_name is 'Имя участника';
+comment on column registration.middle_name is 'Отчество участника';
+comment on column registration.birth_date is 'Дата рождения участника';
+comment on column registration.gender is 'Пол участника';
+comment on column registration.email is 'Адрес электронной почты участника';
+comment on column registration.mobile_phone is 'Номер мобильного телефона участника';
+comment on column registration.delete_reason is 'Причина удаления регистрации';
 
 -- drop tables
 
