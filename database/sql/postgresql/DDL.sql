5e81463bc1a0a3e65a18318b87e17ef9660d579e
Anton Elshin <anton.elshin@mail.ru>
2025-03-04 19:43:56 +0300

    fill DDL operations

--- database/sql/postgresql/DDL.sql
+++ database/sql/postgresql/DDL.sql
@@ -25,3 +25,52 @@ create database development
 -- db.url: jdbc:postgresql://localhost:5432/development
 -- db.username: development_admin
 -- db.password: 123
+
+-- start development
+
+-- create table race
+
+create table if not exists race
+(
+    id bigint,
+    is_active boolean,
+    race_date timestamp without time zone,
+    race_name varchar(100),
+    race_city varchar(100),
+    race_address varchar(200),
+    description text
+);
+
+-- create table distance
+
+create table if not exists distance
+(
+    id bigint,
+    distance_name varchar(4),
+    entrance_fee numeric,
+    entrance_currency varchar(3),
+    racer_limit integer
+);
+
+-- create table registration
+
+create table if not exists registration
+(
+    id bigint,
+    creation_date timestamp without time zone,
+    status varchar(20),
+    last_name varchar(100),
+    first_name varchar(100),
+    middle_name varchar(100),
+    birth_date timestamp without time zone,
+    gender varchar(6),
+    email varchar(100),
+    mobile_phone varchar(10),
+    delete_reason varchar(20)
+);
+
+-- drop tables
+
+drop table race;
+drop table distance;
+drop table registration;
