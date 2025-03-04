91aea4c87e1ec39df255fd0b77aa06f5baf4a8ca
Anton Elshin <anton.elshin@mail.ru>
2025-03-04 20:56:09 +0300

    use sequences with custom names for id generation

--- database/sql/postgresql/DDL.sql
+++ database/sql/postgresql/DDL.sql
@@ -28,23 +28,18 @@ create database development
 
 -- start development
 
--- create table auto_pk_support
+-- create sequence race_seq
 
-create table auto_pk_support
-(
-    table_name varchar(30) primary key,
-    last_id bigint not null
-);
-
-comment on table auto_pk_support is 'Общая таблица идентификаторов';
-comment on column auto_pk_support.table_name is 'Название таблицы';
-comment on column auto_pk_support.last_id is 'Значение последнего идентификатора';
+create sequence race_seq
+    increment 1
+    start 1
+    minvalue 1;
 
 -- create table race
 
 create table if not exists race
 (
-    id bigint primary key,
+    id bigint primary key default nextval('race_seq'),
     is_active boolean not null,
     race_date timestamp without time zone not null,
     race_name varchar(200) not null,
@@ -62,11 +57,18 @@ comment on column race.race_city is 'Город забега';
 comment on column race.race_address is 'Адрес забега';
 comment on column race.description is 'Описание забега';
 
+-- create sequence distance_seq
+
+create sequence distance_seq
+    increment 1
+    start 1
+    minvalue 1;
+
 -- create table distance
 
 create table if not exists distance
 (
-    id bigint primary key,
+    id bigint primary key default nextval('distance_seq'),
     race_id bigint not null references race (id),
     distance_name varchar(4) not null,
     entrance_fee numeric,
@@ -83,11 +85,18 @@ comment on column distance.entrance_fee is 'Сумма вступительно�
 comment on column distance.entrance_currency is 'Валюта вступительного взноса';
 comment on column distance.racer_limit is 'Лимит участников';
 
+-- create sequence registration_seq
+
+create sequence registration_seq
+    increment 1
+    start 1
+    minvalue 1;
+
 -- create table registration
 
 create table if not exists registration
 (
-    id bigint primary key,
+    id bigint primary key default nextval('registration_seq'),
     distance_id bigint not null references distance (id),
     creation_date timestamp without time zone not null,
     status varchar(20) not null,
@@ -117,7 +126,12 @@ comment on column registration.delete_reason is 'Причина удаления
 
 -- drop tables
 
-drop table auto_pk_support;
 drop table registration;
 drop table distance;
 drop table race;
+
+-- drop sequences
+
+drop sequence registration_seq;
+drop sequence distance_seq;
+drop sequence race_seq;
