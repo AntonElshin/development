b62835ae0ece125411273ef77aa8423dacb56fb5
Anton Elshin <anton.elshin@mail.ru>
2025-03-07 00:18:17 +0300

    add primary keys with custom names

--- database/sql/postgresql/DDL.sql
+++ database/sql/postgresql/DDL.sql
@@ -32,13 +32,14 @@ create database development
 
 create table if not exists race
 (
-    id bigint not null unique,
+    id bigint,
     is_active boolean not null,
     race_date timestamp without time zone not null,
     race_name varchar(200) not null,
     race_city varchar(100) not null,
     race_address varchar(200) not null,
-    description text
+    description text,
+    constraint race_id_pk primary key (id)
 );
 
 comment on table race is 'Забег';
@@ -54,12 +55,12 @@ comment on column race.description is 'Описание забега';
 
 create table if not exists distance
 (
-    id bigint not null unique,
+    id bigint,
     distance_name varchar(4) not null,
     entrance_fee numeric,
     entrance_currency varchar(3),
     racer_limit_left integer,
-    constraint distance_id_idx unique (id)
+    constraint distance_id_pk primary key (id)
 );
 
 comment on table distance is 'Дистанция забега';
@@ -73,7 +74,7 @@ comment on column distance.racer_limit_left is 'Оставшийся лимит 
 
 create table if not exists registration
 (
-    id bigint not null unique,
+    id bigint,
     creation_date timestamp without time zone not null,
     status varchar(20) not null,
     last_name varchar(100) not null,
@@ -83,7 +84,8 @@ create table if not exists registration
     gender varchar(6) not null,
     email varchar(100) not null,
     mobile_phone varchar(10) not null,
-    delete_reason varchar(20)
+    delete_reason varchar(20),
+    constraint registration_id_pk primary key (id)
 );
 
 comment on table registration is 'Регистрация на дистанцию забега';
