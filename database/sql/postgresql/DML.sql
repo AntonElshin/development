baf14db25e5f057b92e6258e2b251b296faa8e9d
Anton Elshin <anton.elshin@mail.ru>
2025-03-07 00:33:33 +0300

    use sequences with auto generated names for id generation

--- database/sql/postgresql/DML.sql
+++ database/sql/postgresql/DML.sql
@@ -23,21 +23,15 @@ select * from registration;
 
 -- inserts into race
 
--- select nextval('race_id_seq');
-
 insert into race (is_active, race_date, race_name, race_city, race_address, description) values
     (true, '2025-02-22 12:00:00.000000', 'Забег настоящих мужчин', 'Москва', 'Перовский сквер', null);
 
 -- inserts into distance
 
--- select nextval('distance_id_seq');
-
 insert into distance (race_id, distance_name, entrance_fee, entrance_currency, racer_limit_left) values
     (1, 'FULL', 1400.00, 'RUB', null);
 
 -- inserts into registration
-
--- select nextval('registration_id_seq');
 
 insert into registration (distance_id, creation_date, status, last_name, first_name, middle_name, birth_date, gender, email, mobile_phone, delete_reason) values
     (1, '2025-02-01 15:16:01.000000', 'PENDING_PAYMENT', 'Петров', 'Василий', 'Александрович', to_date('19850417', 'yyyymmdd'), 'MALE', 'petrov_va@mail.ru', '9999999999', null);
