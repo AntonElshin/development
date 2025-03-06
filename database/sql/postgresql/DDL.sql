2d433ce281cad4a0ea2de7880a80b1c163eac427
Anton Elshin <anton.elshin@mail.ru>
2025-03-07 00:25:33 +0300

    add unique constraint for two fields after table creation

--- database/sql/postgresql/DDL.sql
+++ database/sql/postgresql/DDL.sql
@@ -70,6 +70,10 @@ comment on column distance.entrance_fee is 'Сумма вступительно�
 comment on column distance.entrance_currency is 'Валюта вступительного взноса';
 comment on column distance.racer_limit_left is 'Оставшийся лимит участников';
 
+-- alter table distance
+
+alter table distance add constraint distance_race_id_distance_name_idx unique (race_id, distance_name);
+
 -- create table registration
 
 create table if not exists registration
