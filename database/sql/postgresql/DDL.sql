bb518426f88bb19a64ec716dc779b44b8a78c3fc
Anton Elshin <anton.elshin@mail.ru>
2025-03-07 00:05:02 +0300

    fill DDL operations before start development (from devops)

--- database/sql/postgresql/DDL.sql
+++ database/sql/postgresql/DDL.sql
@@ -1,1 +1,27 @@
 -- DDL (Data Definition Language)
+
+-- before start development (from devops)
+
+-- create role (user)
+
+create role development_admin with
+    login
+    nosuperuser
+    createdb
+    nocreaterole
+    inherit
+    noreplication
+    connection limit -1
+    password '123';
+
+-- create database
+
+create database development
+    with
+    owner = development_admin
+    encoding = 'UTF8'
+    connection limit = -1;
+
+-- db.url: jdbc:postgresql://localhost:5432/development
+-- db.username: development_admin
+-- db.password: 123
